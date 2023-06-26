6bb37734b182768f90c11241c8409201a0a3815d
mfirdaus354 <muhammad.anhar.firdausyi@gmail.com>
2023-06-26 11:48:49 +0200

    Update leaders_scraper.py

--- leaders_scraper.py
+++ leaders_scraper.py
@@ -1,96 +1,87 @@
-import sys
-
 import asyncio
 import aiohttp
-import requests
 from bs4 import BeautifulSoup
 import re
+import os
+
 
 root_url = "https://country-leaders.onrender.com"
 cookies_url = f"{root_url}/cookie"
 cookies_dict = dict(cookies_are='working')
-countries_url = f"{root_url}/countries"
 
+async def fetch_url(url, session, cookies=None):
+    # define fetch_url function to query root_url to a particular endpoints
+    async with session.get(url, cookies=cookies) as response:
+        return await response.json()
 
-async def get_countries_and_leaders():
-    async with aiohttp.ClientSession() as session:
-        # Make the cookies request
-        async with session.get(cookies_url, cookies=cookies_dict) as response:
-            ultimate_cookies = response.cookies
+async def get_countries(session, cookies):
+    # define get_countries function to query root_url to "/countries" endpoint, 
+    # then assign and return to querried_countries variable
+    countries_url= f"{root_url}/countries"
+    querried_countries = await fetch_url(countries_url, session, cookies)
+    return querried_countries
 
-        # Make the countries request
-        async with session.get(countries_url, cookies=ultimate_cookies) as response:
-            some_countries = await response.json()
+async def get_leaders(country, session, cookies):
+    # define get_countries function to query root_url to "/leaders" endpoint, 
+    # parse through country in querried_countries
+    # then assign and return to querried_leaders variable as a dictionary
+    leaders_url = f"{root_url}/leaders?country={country}"
+    querried_leaders = await fetch_url(leaders_url, session, cookies)
+    leaders = {country: [{f"{leader['first_name']} {leader['last_name']}": leader['wikipedia_url']} for leader in querried_leaders]}
+    return leaders 
 
-        # Fetch leaders for each country asynchronously
-        tasks = []
-        for country in some_countries:
-            leaders_url = f"{root_url}/leaders?country={country}"
-            task = asyncio.create_task(fetch_leaders(session, leaders_url, ultimate_cookies))
-            tasks.append(task)
+async def scrape_leaders_info(country, leaders, session):
+    summary_file = os.path.dirname(os.path.abspath(__file__))
+    leader_summary = os.path.join(summary_file, "leader-summary.txt")
+    for leader in leaders:
+        for key_people, wikipedia_leader in leader.items():
+            async with session.get(wikipedia_leader) as response:
+                html_content = await response.text()
+                leader_soup = BeautifulSoup(html_content, 'html.parser')
+                header_content = leader_soup.find_all('h2')  
+                paragraph_content = leader_soup.find_all('p')
+                sentences = []
+                for header in header_content:
+                    header_text = header.get_text()
+                    sentences.append(header_text)
+                for paragraph in paragraph_content:
+                    paragraph_text = paragraph.get_text()
+                    sentences.append(paragraph_text)
 
-        # Wait for all leader requests to complete
-        leader_responses = await asyncio.gather(*tasks)
+                main_content = [] 
+                main_content.extend(sentences)  
+                
+                for content in main_content:  
+                    pattern = r'(?:^|\s)([A-Z].*?[.!?])(?=\s|$)'
+                    matches = re.findall(pattern, content)
+                    sentences.extend(matches)  
 
-        final_dict = {}
-        for leaders in leader_responses:
-            for country, leader_names in leaders.items():
-                if country not in final_dict:
-                    final_dict[country] = leader_names
-                else:
-                    final_dict[country].extend(leader_names)
-
-        return final_dict
-
-
-async def fetch_leaders(session, leaders_url, cookies):
-    async with session.get(leaders_url, cookies=cookies) as response:
-        some_leaders = await response.json()
-        leader_names = []
-        for leader in some_leaders:
-            name = leader["first_name"] + " " + leader["last_name"]
-            wikipedia_url = leader["wikipedia_url"].replace("nl", "en")
-            leader_names.append({name: wikipedia_url})
-        return leader_names
-
-
-def extract_sentences(url, tag="p"):
-    response = requests.get(url)
-    leader_soup = BeautifulSoup(response.content, 'html.parser')
-    tag_content = leader_soup.find_all(tag)
-    sentences = []
-    for content in tag_content:
-        text = content.get_text()
-        pattern = r'(?:^|\s)([A-Z].*?[.!?])(?=\s|$)'
-        matches = re.findall(pattern, text)
-        sentences.extend(matches)
-    return sentences
-
-
-async def process_countries():
-    leader_per_country = await get_countries_and_leaders()
-
-    for country, leaders in leader_per_country.items():
-        for leader in leaders:
-            for name, wikipedia_leader in leader.items():
-                leader_summary_file = "C:\\Users\\dimas\\Desktop\\BECODE\\PROJECTS\\project-wikipedia-scrapper\\leader-summary.txt"
-                sentences = extract_sentences(wikipedia_leader)
-
-                with open(leader_summary_file, "a") as leader_story:
-                    leader_story.write(f"""Leaders of {country}
-----------------------------------------------
-{name}
-{sentences}
-----------------------------------------------""")
+                with open(leader_summary, "a", encoding="utf-8") as leader_story:
+                    leader_story.write(f"""
+                                       Leaders of {country}
+                                    ----------------------------------------------
+                                        {key_people}
+                                        {sentences}
+                                    ----------------------------------------------
+                                        """)
 
 
 async def main():
-    await process_countries()
+    async with aiohttp.ClientSession() as session:
+        cookies = await fetch_url(cookies_url, session, cookies_dict)
+        countries = await get_countries(session, cookies)
+        print(countries[0])
 
+        tasks = []
+        for country in countries:
+            tasks.append(asyncio.create_task(get_leaders(country, session, cookies)))
 
+        leader_per_country = {}
+        for task in asyncio.as_completed(tasks):
+            leaders = await task
+            leader_per_country.update(leaders)
 
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+        for country, leaders in leader_per_country.items():
+            await scrape_leaders_info(country, leaders, session)
 
-    
+asyncio.run(main())
